79b9391fca7e7d9c79c52611e30bda7cc2b6d1df
ItsCoderRam <ramnathcse123@gmail.com>
2024-05-18 21:22:06 +0530

    react first project may

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import MarketingData from './components/apicall';
+
+function App() {
+  return (
+    <div className="App">
+      <MarketingData />
+    </div>
+  );
+}
+
+export default App;
